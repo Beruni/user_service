988421d230fcd208d00f221a53db2694317f81ff
Rohit Garg <rohitgarg19@gmail.com>
2016-09-09 12:37:50 +0530

    Printing ENV

--- deploy/push_to_heroku.sh
+++ deploy/push_to_heroku.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 eval "$(docker-machine env default)"
+env
 heroku container:login
 docker tag user_service:v0.0.1 registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
 docker push registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
