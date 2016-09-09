3d266de0c0a7ec7daf67ed7e428e977ee2a040b1
Rohit Garg <rohitgarg19@gmail.com>
2016-09-09 12:33:19 +0530

    Added docker eval command

--- deploy/push_to_heroku.sh
+++ deploy/push_to_heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+eval "$(docker-machine env default)"
 heroku container:login
 docker tag user_service:v0.0.1 registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
 docker push registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
