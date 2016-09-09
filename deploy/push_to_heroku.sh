8e8bff43b5db2e9eee9f73e78f96c8e567af50a2
Rohit Garg <rohitgarg19@gmail.com>
2016-09-09 13:34:39 +0530

    Removing env

--- deploy/push_to_heroku.sh
+++ deploy/push_to_heroku.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 eval "$(docker-machine env default)"
-env
 heroku container:login
 docker tag user_service:v0.0.1 registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web:build_${CI_BUILD_ID}
 docker push registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web:build_${CI_BUILD_ID}
