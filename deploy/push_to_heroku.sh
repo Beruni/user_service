e51d5b81898e77978c796d5253ff5664321e4935
Rohit Garg <rohitgarg19@gmail.com>
2016-09-09 13:33:42 +0530

    Adding build number to deploy tag

--- deploy/push_to_heroku.sh
+++ deploy/push_to_heroku.sh
@@ -3,5 +3,5 @@
 eval "$(docker-machine env default)"
 env
 heroku container:login
-docker tag user_service:v0.0.1 registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
-docker push registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
+docker tag user_service:v0.0.1 registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web:build_${CI_BUILD_ID}
+docker push registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web:build_${CI_BUILD_ID}
