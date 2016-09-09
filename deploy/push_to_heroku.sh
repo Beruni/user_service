04a033c6c092fa60634d35558c035735646a4994
Rohit Garg <rohitgarg19@gmail.com>
2016-09-09 12:30:07 +0530

    Adding deploy stage

---
+++ deploy/push_to_heroku.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+heroku container:login
+docker tag user_service:v0.0.1 registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
+docker push registry.heroku.com/${CUSTOM_ENVIRONMENT}user-beruni/web
